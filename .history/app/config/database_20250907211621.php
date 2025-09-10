deb74361d1e6a3e34381884c29937d800dd77545
nglkrs <ronquilloangelkris@gmail.com>
2025-09-10 16:31:10 +0800

    First Commit

--- .history/app/config/database_20250907211621.php
+++ .history/app/config/database_20250907211621.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'sql12.freesqldatabase.com',
+    'hostname'	=> 'localhost',
     'port'		=> '3306',
-    'username'	=> 'sql12798034',
-    'password'	=> 'R4nNpJuL5n',
-    'database'	=> 'sql12798034',
+    'username'	=> 'root',
+    'password'	=> '',
+    'database'	=> 'angel',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
